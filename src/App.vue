fdfafc4606689b0be60ef32d669902acc8a36577
Rajat <rdbiswas97@gmail.com>
2019-04-26 18:25:28 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="MNNIT Logo" width="150" src="./assets/mnnit-logo.png">
+    <Login msg="MNNIT Library Management System"/>
+  </div>
+</template>
+
+<script>
+import Login from './components/Login.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Login
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 10%;
+}
+</style>
---
+++ src/components/Login.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="login">
+    <br>
+    <h3>Welcome to</h3>
+    <h1>{{ msg }}</h1>
+    <br>
+    <v-form>
+      <v-container>
+        <v-layout row wrap align-center justify-center>
+          <v-flex xs12 sm6 md3>
+            <v-text-field
+              label="IP Address"
+            ></v-text-field>
+          </v-flex>
+          <v-flex xs12 sm6 md3>
+            <v-text-field
+              label="Database Name"
+            ></v-text-field>
+          </v-flex>
+          <v-flex xs12 sm6 md3>
+            <v-text-field
+              label="Database Username"
+            ></v-text-field>
+          </v-flex>
+          <v-flex xs12 sm6 md3>
+            <v-text-field
+              v-model="password"
+              :append-icon="show ? 'visibility' : 'visibility_off'"
+              :rules="[rules.required, rules.min]"
+              :type="show ? 'text' : 'password'"
+              name="input-10-1"
+              label="Database Password"
+              hint="At least 8 characters"
+              counter
+              @click:append="show = !show"
+            ></v-text-field>
+          </v-flex>
+          <br>
+          <v-flex xs12 sm4 text-xs-center>
+            <div>
+              <v-btn depressed>Login</v-btn>
+            </div>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  props: {
+    msg: String
+  },
+  data () {
+    return {
+      show: false,
+      password: '',
+      rules: {
+        required: value => !!value || 'Required.',
+        min: v => v.length >= 8 || 'Min 8 characters',
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
